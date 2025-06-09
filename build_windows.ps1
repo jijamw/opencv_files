2e270ba0b9f9a99c937d42a5861a3f470b960f7d
jijamw <jijamw@outlook.com>
2025-06-09 20:46:45 +0800

    Set FREETYPE=ON Test

--- build_windows.ps1
+++ build_windows.ps1
@@ -77,12 +77,17 @@ function BuildForWindows($targetPlatform, $vcpkgPath, $runMsbuild, $hostPlatform
         -D WITH_MSMF=${msmfFlag} `
         -D WITH_MSMF_DXVA=${msmfFlag} `
         -D WITH_QT=OFF `
-        -D WITH_FREETYPE=OFF `
+        -D WITH_FREETYPE=ON  `
+        -D WITH_HARFBUZZ=ON  `
         -D WITH_TESSERACT=ON `
         -D WITH_CAROTENE=${caroteneFlag} `
         -D Tesseract_INCLUDE_DIR="${vcpkgPath}/installed/${targetPlatform}-windows-static/include" `
         -D Tesseract_LIBRARY="${vcpkgPath}/installed/${targetPlatform}-windows-static/lib/tesseract41.lib" `
         -D Lept_LIBRARY="${vcpkgPath}/installed/${targetPlatform}-windows-static/lib/leptonica-1.81.0.lib" `
+        -D FREETYPE_INCLUDE_DIRS="${vcpkgPath}/installed/${targetPlatform}-windows-static/include" `
+        -D FREETYPE_LIBRARIES="${vcpkgPath}/installed/${targetPlatform}-windows-static/lib/freetype.lib" `
+        -D HARFBUZZ_INCLUDE_DIRS="${vcpkgPath}/installed/${targetPlatform}-windows-static/include" `
+        -D HARFBUZZ_LIBRARIES="${vcpkgPath}/installed/${targetPlatform}-windows-static/lib/harfbuzz.lib" ` 
         -D ENABLE_CXX11=1 `
         -D OPENCV_ENABLE_NONFREE=ON `
         -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules `
@@ -112,6 +117,8 @@ If ((Resolve-Path -Path $MyInvocation.InvocationName).ProviderPath -eq $MyInvoca
     #$platform = "arm64"
 
     Invoke-Expression "${vcpkgPath}\vcpkg.exe install tesseract:${platform}-windows-static" -ErrorAction Stop
+    Invoke-Expression "${vcpkgPath}\vcpkg.exe install freetype:${platform}-windows-static" -ErrorAction Stop
+    Invoke-Expression "${vcpkgPath}\vcpkg.exe install harfbuzz:${platform}-windows-static" -ErrorAction Stop
     #Invoke-Expression "${vcpkgPath}\vcpkg.exe integrate install" -ErrorAction Stop
 
     BuildForWindows $platform $vcpkgPath $FALSE
